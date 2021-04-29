6dbe45a556d1c1fce7cb294a2a1476b202ec236e
Andrey Belskiy <diksandab@gmail.com>
2021-04-30 00:05:41 +0600

    Added lessons for 1 section

---
+++ step_1/lesson_6_step_2_examples.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.get('http://suninjuly.github.io/simple_form_find_task.html')
+button1 = browser.find_element_by_id('submit_button')
+button2 = browser.find_element(By.ID, 'submit_button')
